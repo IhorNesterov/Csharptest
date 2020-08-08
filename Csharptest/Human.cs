cd48fe78bd8cfd1875f284f353f1ca07ab9bcec0
ihor nesterov <igornes190@gmail.com>
2020-08-08 22:37:43 +0300

    Created class Human

---
+++ Csharptest/Human.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Csharptest
+{
+    class Human
+    {
+        private int age;
+        private int weight;
+        private string name;
+
+        Human()
+        {
+            age = 20;
+            weight = 70;
+            name = "Dodik";
+        }
+
+        public Human(int age, int weight, string name)
+        {
+            SetAge(age);
+            SetWeight(weight);
+            SetName(name);
+        }
+
+        public void SetAge(int age)
+        {
+            if (age > 0 && age < 150)
+            {
+                this.age = age;
+            }
+        }
+        public int GetAge()
+        {
+            return age;
+        }
+
+        public void SetWeight(int weight)
+        {
+            if (weight > 10 && age < 400)
+            {
+                this.weight = weight;
+            }
+        }
+        public int GetWeight()
+        {
+            return weight;
+        }
+
+        public void SetName(string name)
+        {
+            if (name != null)
+            {
+                this.name = name;
+            }
+        }
+        public string GetName()
+        {
+            return name;
+        }
+    }
+
+}
---
+++ Csharptest/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+namespace Csharptest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human human = new Human(20, 90, "Igor Nesterov"); //Create new object from class Human
+            Console.WriteLine("Hello,write name)");
+            string input = Console.ReadLine(); 
+            human.SetName(input);
+            Console.WriteLine("Write weight");
+            input = Console.ReadLine();
+            human.SetWeight(Int32.Parse(input));
+            Console.WriteLine("Write age");
+            input = Console.ReadLine();
+            human.SetAge(Int32.Parse(input));
+            Console.WriteLine("Name = " + human.GetName());
+            Console.WriteLine("Age = " + human.GetAge());
+            Console.WriteLine("Weight = " + human.GetWeight());
+        }
+    }
+}
