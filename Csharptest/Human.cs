1d70d43b5e42e959bbc43ce3676bfeb232dfb38b
ihor nesterov <igornes190@gmail.com>
2020-08-09 22:23:19 +0300

    Added parse method for string

--- Csharptest/Human.cs
+++ Csharptest/Human.cs
@@ -93,6 +93,7 @@ namespace Csharptest
     class Student : Human
     {
         private int[] marks;
+        private string[] subjects;
         private string university_name;
         
         public Student() : base()
@@ -101,13 +102,8 @@ namespace Csharptest
             this.university_name = "Default university name";
         }
 
-        public Student(int age, int weight, string name,string univer_name,int[] mark) : base(age,weight,name)
+        public Student(int age, int weight, string name,string univer_name) : base(age,weight,name)
         {
-            if (mark != null)
-            {
-                this.marks = mark;
-            }
-
             if (univer_name != null)
             {
                 this.university_name = univer_name;
@@ -126,6 +122,41 @@ namespace Csharptest
             return this.university_name;
         }
 
+        public void SetSubjects(string[] subjects)
+        {
+            if (subjects != null)
+            {
+                this.subjects = subjects;
+            }
+        }
+        public string[] GetSubject()
+        {
+            return this.subjects;
+        }
+
+        public string GetSubject(int pos)
+        {
+            return this.subjects[pos];
+        }
+
+        public int[] GetMarks()
+        {
+            return this.marks;
+        }
+
+        public int GetMarks(int pos)
+        {
+            return this.marks[pos];
+        }
+
+        public void Setmarks(int[] mark)
+        {
+            if(mark != null)
+            {
+                this.marks = mark;
+            }
+        }
+
         public override string Display()
         {
             string output = base.Display();
@@ -133,7 +164,7 @@ namespace Csharptest
             output += "\nMarks:\n";
             for(int i = 0; i < this.marks.Length; i++)
             {
-                output += this.marks[i] + "\n";
+                output += this.GetSubject(i) + ":" + this.marks[i] + "\n";
             }
             return output;
         }
@@ -143,8 +174,63 @@ namespace Csharptest
             base.Write();
             Console.WriteLine("\nWrite university name");
             this.SetUniversityName(Console.ReadLine());
+            
+        }
+
+        public string[] Parse(string input)
+        {
+            int wordscount = 0;
+
+            for(int i = 0;i < input.Length;i++)
+            {
+                //Console.WriteLine("Index:" + i);
+                //Console.WriteLine("Char:" + input.Substring(i, 1));
+                if (input.Substring(i, 1) == " ")
+                {
+                    wordscount++;
+                }
+            }
+            //Console.WriteLine("wordscount:" + wordscount);
+            int[] startpoints = new int[wordscount];
+            int[] endpoints = new int[wordscount + 1];
+            string[] output = new string[wordscount];
+            int x = 1;
+            endpoints[0] = 0;
+            
+            for (int k = 0; k < input.Length; k++)
+            {
+                if (input.Substring(k, 1) == " ")
+                {
+                    endpoints[x] = k + 1;
+                    //Console.WriteLine("Point:" + k);
+                    //Console.WriteLine("Endd:" + endpoints[x]);
+                    x++;
+                }
+            }
+            /*
+            for(int i = 0; i < endpoints.Length; i++)
+            {
+                Console.WriteLine("Ended:" + endpoints[i]);
+            }
+            */
+            for(int i = 1;i < wordscount + 1; i++)
+            {
+                output[i - 1] = input.Substring(endpoints[i-1], endpoints[i] - endpoints[i - 1]);
+                //Console.WriteLine("Endpoint:" + endpoints[i - 1]);
+                //Console.WriteLine("String:" + output[i - 1]);
+            }
+
+            /*for (int i = 0; i < wordscount; i++)
+            {
+                Console.WriteLine("parsed:" + output[i]);
+            }
+            */
+            return output;
         }
     }
+
+
+
 }
 
 
--- Csharptest/Program.cs
+++ Csharptest/Program.cs
@@ -9,9 +9,9 @@ namespace Csharptest
         {
             int[] mark = { 3, 3, 4, 5, 4, 5 };
             Human human = new Human(20, 90, "Igor Nesterov"); //Create new object from class Human
-            Student student = new Student(19, 60, "True Dolboeb", "Politekh", mark);
-            human.Write();
-            student.Write();
+            Student student = new Student(19, 60, "True Dolboeb", "Politekh");
+            student.SetSubjects(student.Parse("Khimia Phisics Math Sport Ukr Eng "));
+            student.Setmarks(mark);
             Console.WriteLine(human.Display());
             Console.WriteLine(student.Display());
         }
